339054dd6d80b34bdc89103db44367021defab92
GrxB <gricsb@gmail.com>
2023-06-17 21:20:42 +0200

    feat(messages): rpc messages with json parse and format print with their associated command options

--- src/httpclientconnector.hpp
+++ src/httpclientconnector.hpp
@@ -1,62 +1,49 @@
 #ifndef HTTP_CLIENT_CONNECTOR_H
 #define HTTP_CLIENT_CONNECTOR_H
 
-#ifndef BOOST_BEAST_USE_STD_STRING_VIEW
-#define BOOST_BEAST_USE_STD_STRING_VIEW
-#endif
-
-#include <string>
-#include <string_view>
-#include <unordered_map>
-
 #ifndef CPPHTTPLIB_ALLOW_LF_AS_LINE_TERMINATOR
 #define CPPHTTPLIB_ALLOW_LF_AS_LINE_TERMINATOR
 #endif
 
-#include "httplib.h"
-#include "jsonrpccxx/iclientconnector.hpp"
-#include "jsonrpccxx/common.hpp"
+#include <string>
 
-namespace staff{
-using std::string;
-using std::string_view;
+#include "httplib.h"
+#include "jsonrpccxx/common.hpp"
+#include "jsonrpccxx/iclientconnector.hpp"
+//#include <string_view>
+
+namespace staff {
 using httplib::Headers;
 using jsonrpccxx::IClientConnector;
 using jsonrpccxx::JsonRpcException;
+using std::string;
+using std::string_view;
 
 class HttpClientConnector : public IClientConnector {
     httplib::Client _client;
     string          _path;
     Headers         _hdrs;
-public:
-    explicit HttpClientConnector(
-        string        hostname,
-        int           port,
-        string        base_path
-    ) : _client{hostname.c_str(), port}, 
-        _path{base_path}
-    {
-        _hdrs = {
-            {"Host", std::move(hostname)},
-            {"Connection", "keep-alive"}
-        };
+
+   public:
+    explicit HttpClientConnector(string hostname, int port, string base_path)
+        : _client{hostname.c_str(), port}, _path{base_path} {
+        _hdrs = {{"Host", std::move(hostname)}, {"Connection", "keep-alive"}};
     }
 
-    string Send(const string &message) override {
+    string Send(const string& message) override {
         _client.set_default_headers(_hdrs);
         auto res = _client.Post(_path, message, "application/json");
         if (!res || res->status != 200)
             throw jsonrpccxx::JsonRpcException(
                 -32003, "client connector error, received status != 200");
-        
+
         return res->body;
     }
 
     void set_default_headers(Headers headers) {
-        for (auto [k, v] : headers)
-            _hdrs.insert({std::move(k), std::move(v)});
+        for (auto& [k, v] : headers) _hdrs.insert({std::move(k), std::move(v)});
     }
 };
-} // end namespace staff
+}  // end namespace staff
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,32 @@
-#include <memory>
+#include <sysexits.h>
+
+#include <cstdlib>
 #include <exception>
+#include <map>
+#include <memory>
 #include <string>
 #include <string_view>
-#include <cstdlib>
-#include <sysexits.h>
-#include <map>
 
-#include "fmt/core.h"
-#include "spdlog/spdlog.h"
 #include "cxxopts.hpp"
-#include "jsonrpccxx/client.hpp"
-#include "nlohmann/json.hpp"
-
+#include "fmt/core.h"
 #include "httpclientconnector.hpp"
+#include "jsonrpccxx/client.hpp"
+#include "messages.hpp"
+#include "nlohmann/json.hpp"
+#include "spdlog/spdlog.h"
 #include "string_trim.hpp"
 
 using std::string;
 using namespace std::string_view_literals;
 using cxxopts::ParseResult;
 using jsonrpccxx::version;
-using json = nlohmann::json;
+using json       = nlohmann::json;
 using JsonObject = std::map<string, json>;
 
+using namespace messages;
+
 constexpr auto DEFAULT_VTOKEN =
-"KSDHSDFOGQ5WERYTUIQWERTYUISDFG1HJZXCVCXBN2GDSMNDHKVKFsVBNf"sv;
+    "KSDHSDFOGQ5WERYTUIQWERTYUISDFG1HJZXCVCXBN2GDSMNDHKVKFsVBNf"sv;
 
 struct Args {
     spdlog::level::level_enum log_level;
@@ -31,10 +34,17 @@ struct Args {
     int                       port;
     string                    base_path;
     string                    verify_token;
+    bool                      detailed;
+    bool                      system_info;
+    bool                      system_status;
+    bool                      sms_storage_state;
+    int                       sms_contact_list;
 };
-std::unique_ptr<Args> parse(int argc, const char* argv[]);
+std::unique_ptr<Args> parse(int argc, const char *argv[]);
 
-int main(int argc, const char* argv[]) {
+staff::HttpClientConnector DefaultClient(Args &args);
+
+int main(int argc, const char *argv[]) {
     spdlog::info("asmsd - Alcatel (dongle) SMS Daemon");
     spdlog::flush_on(spdlog::level::err);
     spdlog::flush_on(spdlog::level::critical);
@@ -51,34 +61,57 @@ int main(int argc, const char* argv[]) {
 
     if (spdlog::get_level() != args->log_level) {
         spdlog::set_level(args->log_level);
-        spdlog::info("log levelt seted to: {}",
-            spdlog::level::to_string_view(spdlog::get_level()));
+        spdlog::info("log levelt set to: {}",
+                     spdlog::level::to_string_view(spdlog::get_level()));
     }
 
     try {
-        spdlog::info("connecting to host: {}, with: {}",
-            args->host, args->port);
+        spdlog::info("try connect to host: {}, with: {}", args->host,
+                     args->port);
 
-        staff::HttpClientConnector http_client{
-            args->host,
-            args->port,
-            std::move(args->base_path)
-        };
-        http_client.set_default_headers({
-            {"Content-Type", "application/json"},
-            {"_TclRequestVerificationKey", std::move(args->verify_token)},
-            {"_TclRequestVerificationToken", "null"},
-            {"Referer", fmt::format("http://{}/index.html", args->host)},
-            {"Origin", std::move(args->host)},
-        });
-
+        auto                      http_client = DefaultClient(*args);
         jsonrpccxx::JsonRpcClient client(http_client, version::v2);
-        const auto ret = client.CallMethod<JsonObject>("1", "GetSystemInfo");
 
-        fmt::print("RESPONSE:\n");
-        for (const auto& [k, v] : ret)
-            fmt::print("\t{}: \t{}\n", k, v);
+        bool specified{};
 
+        if (args->system_info) {
+            const auto sys_inf =
+                client.CallMethod<SystemInfo>("1", SystemInfo::query_str);
+            if (args->detailed)
+                fmt::print("{:d}\n", sys_inf);
+            else
+                fmt::print("{}\n", sys_inf);
+            specified = true;
+        }
+
+        if (args->system_status) {
+            const auto sys_status =
+                client.CallMethod<SystemStatus>("2", SystemStatus::query_str);
+            if (args->detailed)
+                fmt::print("{:d}\n", sys_status);
+            else
+                fmt::print("{}\n", sys_status);
+            specified = true;
+        }
+
+        if (args->sms_storage_state) {
+            const auto sms_state = client.CallMethod<SmsStorageState>(
+                "3", SmsStorageState::query_str);
+            fmt::print("{}\n", sms_state);
+            specified = true;
+        }
+
+        if (args->sms_contact_list > 0) {
+            const auto sms_clist = client.CallMethodNamed<SmsContactList>(
+                "4", SmsContactList::query_str,
+                Page(args->sms_contact_list - 1));
+            fmt::print("{}\n", sms_clist);
+            specified = true;
+        }
+
+        if (!specified)
+            fmt::print(stderr,
+                       "no command specified, see usage: asmsd --help\n");
 
     } catch (const std::exception &e) {
         spdlog::critical("json rpc request error: {}", e.what());
@@ -88,31 +121,40 @@ int main(int argc, const char* argv[]) {
     std::exit(EXIT_SUCCESS);
 }
 
-std::unique_ptr<Args> parse(int argc, const char* argv[]) {
+std::unique_ptr<Args> parse(int argc, const char *argv[]) {
     using cxxopts::Options;
     using cxxopts::value;
 
-    auto options_allocated = 
-        std::make_unique<Options>("asmsd", " - alcatel sms daemon");
-    auto& opts = *options_allocated;
+    auto options_allocated =
+        std::make_unique<Options>("asmsd", "ASMSD - Alcatel SMS Daemon");
+    auto &opts = *options_allocated;
 
-    auto args = std::make_unique<Args>();
+    auto   args = std::make_unique<Args>();
     string log_level{};
-    opts.add_options()
-    ("l,log-level", "log level", 
-        value(log_level)->default_value("info"))
-    ("h,host", "hostname or ip address of the device", 
-        value(args->host)->default_value("localhost"))
-    ("p,port", "target port number of the http connection", 
-        value(args->port)->default_value("80"))
-    ("b,base-path", "base path of the json rpc endpoint",
-        value(args->base_path)->default_value("/jrd/webapi"))
-    ("v,verify-token", "verification token for the rpc requests",
-        value(args->verify_token)->default_value(string(DEFAULT_VTOKEN)))
-    ("help", "print usage");
+    opts.set_width(90).add_options()  //
+        ("l,log-level", "log level", value(log_level)->default_value("info"))(
+            "h,host", "hostname or ip address of the device",
+            value(args->host)->default_value("localhost"))(
+            "p,port", "target port number of the http connection",
+            value(args->port)->default_value("80"))(
+            "b,base-path", "base path of the json rpc endpoint",
+            value(args->base_path)->default_value("/jrd/webapi"))(
+            "v,verify-token", "verification token for the rpc requests",
+            value(args->verify_token)->default_value(string(DEFAULT_VTOKEN)))(
+            "help", "print usage");
 
-    auto result = 
-        std::make_unique<ParseResult>(opts.parse(argc, argv));
+    opts.add_options("commands")  //
+        ("system-info", "show system informations", value(args->system_info))(
+            "system-status", "show network connection status",
+            value(args->system_status))("sms-storage-state",
+                                        "show SMS storage counters",
+                                        value(args->sms_storage_state))(
+            "sms-contact-list", "show SMS contacts with last message",
+            value(args->sms_contact_list)->implicit_value("1"))(
+            "detailed", "show information views in deatiled mode",
+            value(args->detailed));
+
+    auto result = std::make_unique<ParseResult>(opts.parse(argc, argv));
 
     if (result->count("help")) {
         fmt::print("{}", opts.help());
@@ -127,4 +169,17 @@ std::unique_ptr<Args> parse(int argc, const char* argv[]) {
     args->log_level = spdlog::level::from_str(log_level);
 
     return args;
+}
+
+staff::HttpClientConnector DefaultClient(Args &args) {
+    staff::HttpClientConnector http_client{args.host, args.port,
+                                           std::move(args.base_path)};
+    http_client.set_default_headers({
+        {"Content-Type", "application/json"},
+        {"_TclRequestVerificationKey", std::move(args.verify_token)},
+        {"_TclRequestVerificationToken", "null"},
+        {"Referer", fmt::format("http://{}/index.html", args.host)},
+        {"Origin", std::move(args.host)},
+    });
+    return http_client;
 }
